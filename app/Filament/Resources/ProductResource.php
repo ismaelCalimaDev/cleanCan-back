2578da479d9f7f4f2ff9581d31b5468eaebdfd83
IsmaelSantiagoR <santiago@calimasolutions.com>
2022-07-14 21:41:29 +0100

    *add products admin panel

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Mohamedsabil83\FilamentFormsTinyeditor\Components\TinyEditor;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $label = 'Lavado';
+    protected static ?string $pluralLabel = 'Lavados';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->label('Lavado')
+                    ->required(),
+                TinyEditor::make('description')
+                    ->label('Descripción')
+                    ->required()
+                    ->columnSpan(2),
+                Forms\Components\TextInput::make('price')
+                    ->label('Precio')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')->label('Tipo de Lavado')->searchable(),
+                Tables\Columns\TextColumn::make('price')->label('Precio')->money('eur'),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
