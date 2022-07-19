6f691742c29199b837425c26ea6f27c551577475
IsmaelSantiagoR <santiago@calimasolutions.com>
2022-07-19 16:48:17 +0100

    * change to longtext product description

--- database/migrations/2022_07_12_161925_create_products_table.php
+++ database/migrations/2022_07_12_161925_create_products_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('description');
+            $table->longText('title');
+            $table->longText('description');
             $table->string('price');
             $table->timestamps();
         });
